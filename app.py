70003591001b731f3917c872a7543282ef9e959c
KidSmithy <hanshuennyuen@gmail.com>
2025-02-10 20:39:28 +0800

    page

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import os
 import streamlit as st
 from google import genai
+
+st.set_page_config(
+    page_title="SUTD AI Ig page",
+    page_icon=":robot_face:",  # Favicon emoji
+    layout="wide",  # Page layout option
+)
+
+st.title("Welcome to SUTD AI Ig page")
